5e9128211416ab53cf95874e966a0c7321df4c93
sphinx-austin <austinjuniour4@gmail.com>
2023-08-16 15:32:49 +0300

    list api view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render
 
+# third party imports
+from django.views.generic import ListView
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from core.models import Keyword
+
 # Create your views here.
-def base(request):
-    return render(request, 'core/base.html')
+class AllKeywordsView(ListView):
+    model = Keyword
+    template_name = "core/base.html"
+
+
+class KeywordListView(ListView):
+    paginate_by = 5
+    model = Keyword
+
+def listing(request, page):
+    keywords = Keyword.objects.all().order_by("name")
+    paginator = Paginator(keywords, 2)
+    page_object = paginator.get_page(page)
+    page_object.adjusted_elided_pages = paginator.get_elided_page_range(page)
+    context = {"page_obj": page_object}
+    return render(request, "core/keyword_list.html", context)
+
+
+def listing_api(request):
+    page_number = request.GET.get("page", 1)
+    per_page = request.GET.get("per_page", 2)
+    startswith = request.GET.get("startswith", "")
+    keywords = Keyword.objects.filter(name__startswith=startswith)
+    paginator = Paginator(keywords, per_page)
+    page_obj = paginator.get_page(page_number)
+    data = [{"name": kw.name} for kw in page_obj.object_list]
+
+    payload = {
+        "page": {
+            "current": page_obj.number,
+            "has_next": page_obj.has_next(),
+            "has_previous": page_obj.has_previous(),
+        },
+        "data": data,
+    }
+    return JsonResponse(payload)
